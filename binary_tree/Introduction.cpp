8b5a4a198c5c192d5eb344ef867427dac3e73d4a
yashasvi sakure <yashasvisakure@gmail.com>
2021-07-24 22:40:34 +0530

    adding files

---
+++ binary_tree/Introduction.cpp
@@ -0,0 +1,25 @@
+
+// binary tree is a hirarchial data structure with childrens to each parents 
+// each node in binary tree have left and right referances along data element
+//topmost node is root 
+//elements with no children are leaves
+//example file system in computer
+//* full binary tree each tree have either 2 or 0 childern
+//* completely filled binary tree all levels are filled execpt last one 
+//* perfect binary tree (symmetry ) all nodes have two children and laeves are at same level
+// *balanced bianry tree height = o(log n)
+//degenerate one child only (pathalogical)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
