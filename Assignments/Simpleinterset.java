5e4160aae314de3028ddf426d947dc4ac2d42485
yashasvi sakure <yashasvisakure@gmail.com>
2021-09-09 20:17:53 +0530

    commit changes

---
+++ Assignments/Simpleinterset.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Simpleinterset {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int p  = in.nextInt();
+        int t  = in.nextInt();
+        int r  = in.nextInt();
+        in.close();
+        if(true){
+            int ans=0;
+            ans = p*1+p*r*t;
+            System.out.println(ans);
+        }
+    }   
+}
