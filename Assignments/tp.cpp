6bde219f2f447da2d097d893ae500df727eef758
yashasvi sakure <yashasvisakure@gmail.com>
2021-09-14 17:00:24 +0530

    commit

---
+++ Assignments/tp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, sum;
+    int arr[n];
+    for(int i=0;i<n;i++){ 
+        cin >> arr[i];
+        sum += arr[i];
+}
+    
+    cout << sum << endl;
+
+}
